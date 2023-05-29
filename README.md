e94fa6316156679ed812656ea7879d740b9553b8
Peter Jindra <peterfjindra@gmail.com>
2023-05-29 10:17:11 -0400

    rename app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BarcartUi
+# Heardle2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'barcart-ui'`, () => {
+  it(`should have as title 'heardle2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('barcart-ui');
+    expect(app.title).toEqual('heardle2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('barcart-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('heardle2 app is running!');
   });
 });
